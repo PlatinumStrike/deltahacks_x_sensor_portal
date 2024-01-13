9a5459773a378249566a6399bef85e35d16a371a
Justin Hanna <hanna.jh1@gmail.com>
2024-01-13 16:02:26 -0500

    implement fonts class

---
+++ src/app/resources/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Bebas_Neue, Lexend_Deca } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lexend_deca = Lexend_Deca({ subsets: ["latin"] });
