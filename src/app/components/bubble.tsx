3046e9977ee250bd20fd40534a5dcac0d99197fb
Justin Hanna <hanna.jh1@gmail.com>
2024-01-13 22:39:20 -0500

    update bubble structure

--- src/app/components/bubble.tsx
+++ src/app/components/bubble.tsx
@@ -1,11 +1,17 @@
 import { Children } from "react";
 
-export default function Bubble(props: { children: any }) {
+export default function Bubble(props: { className?: string; children: any }) {
   return (
-    <div className="grow flex rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 m-14">
-      <p className="grow flex justify-center items-center">
+    <div
+      className={
+        "grow flex rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 m-14"
+      }
+    >
+      <div
+        className={"grow flex justify-center items-center " + props.className}
+      >
         {Children.toArray(props.children)}
-      </p>
+      </div>
     </div>
   );
 }
